2c5ab1501124e4025cffe02f459770ad5354d0b8
Edwin Njeru <mailnjeru@gmail.com>
2019-04-29 18:43:07 +0300

    Initial application generated by JHipster-5.8.2

---
+++ src/main/java/io/github/preps/service/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package io.github.preps.service.config.audit;
---
+++ src/test/java/io/github/preps/service/cucumber/stepdefs/StepDefs.java
@@ -0,0 +1,9 @@
+package io.github.preps.service.cucumber.stepdefs;
+
+import org.springframework.test.web.servlet.ResultActions;
+
+public abstract class StepDefs {
+
+    protected ResultActions actions;
+
+}
